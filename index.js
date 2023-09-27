49accbf59eb5b26dfa5a365d164caaa4eb426a72
Uzmakh <urbazoha@gmail.com>
2023-09-27 16:17:04 +0500

    Defining Routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { config } = require("dotenv");
 const Note = require("./models/Notes");
 config(); // Load environment variables from .env file
 
-connectToMongodb()
+connectToMongodb();
 const app = express();        //initialize
 const PORT = process.env.PORT || 5000;
 
@@ -14,15 +14,22 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // default route
-app.get("/api/contact", (req, res) => {
-    res.send("Hello from Node.js Backend server!");
+app.get("/", (req, res) => {
+    res.send("Hello working!");
+});
+// Just Examples for routes
+app.get("/api/v1/signup", (req, res) => {
+    res.send("Hello SignUp!");
+});
+app.get("/api/v1/login", (req, res) => {
+    res.send("Hello LogIn!");
 });
 
 // Available routes
 //  get notes
-// app.use('/api/v1/Notes', require('./routes/notesRoutes/getNotesRoute'));
+app.use('/api/v1/', require('./routes/notesRoutes/getNotesRoute'));
 // create notes
-app.use('/api/notes', require('./routes/notesRoutes/createNotesRoute'));
+app.use('/api/v1/', require('./routes/notesRoutes/createNotesRoute'));
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port http://localhost:${PORT}`);
--- routes/notesRoutes/getNotesRoute.js
+++ routes/notesRoutes/getNotesRoute.js
@@ -1,15 +1,22 @@
-// const express = require('express');
-// const User = require('../../models/User');
-// const router = express.Router();
-// const { body } = require('express-validator');
+const express = require('express');
+const User = require('../../models/Users');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
 // const createNotesController = require('../../controllers/notesControllers/createNotesController');
 
 
-// // Create a User using : POST "/api/auth/" (doesn't require auth)
-// router.post('/', (req, res) => {
-//     console.log(req.body);
-//     const user = User(req.body);
-//     user.save();
-//     res.send(req.body);
-// })
-// module.exports = router
+// Create a User using: POST "/api/getNotesRoute/"(doesn't require auth)
+router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 6 })
+], (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(req.body);
+
+})
+module.exports = router
